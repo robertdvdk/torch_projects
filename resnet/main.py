b86a5d4ee88067fb53e3f650fe16bb8ce1b33356
robert.vanderklis <robert.vanderklis@wur.nl>
2023-11-10 16:42:47 +0100

    Added summarywriter

--- resnet/main.py
+++ resnet/main.py
@@ -1,26 +1,33 @@
 # Import statements
 import torch
 import torch.nn as nn
-import torchvision.transforms.v2 as transforms
+from torch.utils.tensorboard import SummaryWriter
 import torch.utils.data
+import datasets
 import model
 import time, gc
 import os
-import numpy as np
 from tqdm import tqdm
 import argparse
-import torchvision
 
 # Function definitions
-def val(net, loader, device):
-    accs = []
-    for x, t in loader:
-        x, t = x.to(device), t.to(device)
-        preds = torch.argmax(net(x), dim=-1)
-        accs.append((preds == t).float().mean().cpu().numpy())
-    return np.mean(accs)
+def mean(lst):
+    return round(sum(lst) / len(lst), 3)
 
-def train_batch(X, y, net, loss_fn, optimizer, device, scaler=None):
+def val(X, y, net, device, dataset):
+    X, y = X.to(device), y.to(device)
+    preds = net(X)
+    return calculate_accuracy(preds, y, 'top1' if dataset == 'cifar10' else 'top5')
+
+def calculate_accuracy(preds, y, metric='top1'):
+    if metric == 'top1':
+        return (torch.argmax(preds, dim=-1) == y).float().mean().cpu().numpy()
+    top5 = torch.sort(preds, dim=-1, descending=True)[1][:, :5]
+    y_stack = torch.stack((y, y, y, y, y), dim=-1)
+    return torch.where(top5 == y_stack, 1., 0.).sum(dim=-1).mean().cpu().numpy()
+
+
+def train_batch(X, y, net, loss_fn, optimizer, device, dataset, scaler=None):
     X, y = X.to(device), y.to(device)
     optimizer.zero_grad()
     if not scaler:
@@ -35,56 +42,83 @@ def train_batch(X, y, net, loss_fn, optimizer, device, scaler=None):
         scaler.scale(loss).backward()
         scaler.step(optimizer)
         scaler.update()
-    return (torch.argmax(preds, dim=-1) == y).float().mean().cpu().numpy()
+    return calculate_accuracy(preds, y, 'top1' if dataset == 'cifar10' else 'top5'), loss.item()
 
-def train(net, trainloader, valloader, device, optimizer, num_epochs, scaler, model_name):
+def train(net, trainloader, valloader, device, optimizer, num_epochs, scheduler,
+          model_name, dataset, writer, scaler=None):
     loss_fn = nn.CrossEntropyLoss()
     for epoch in range(num_epochs):
-        accs = []
+        train_accs, val_accs, losses = [], [], []
         start = time.time()
-        for i, (X, y) in enumerate(tqdm(trainloader)):
-            correct = train_batch(X, y, net, loss_fn, optimizer, device, scaler)
-            accs.append(correct)
-        train_acc = np.mean(accs)
+        for X, y in tqdm(trainloader):
+            train_acc, loss = train_batch(X, y, net, loss_fn, optimizer, device, dataset, scaler)
+            train_accs.append(train_acc)
+            losses.append(loss)
+
         with torch.no_grad():
-            val_acc = val(net, valloader, device)
-        time_taken = time.time() - start
-        with open(f'./results/{model_name}.txt', 'a') as fopen:
-            fopen.write(f'EPOCH {epoch}\nTime: {time_taken}\nTrain accuracy: {train_acc}\nValidation accuracy: {val_acc}\n')
-        torch.save(net.state_dict(), f'./{model_name}.pt')
+            for X, y in tqdm(valloader):
+                val_acc = val(X, y, net, device, dataset)
+                val_accs.append(val_acc)
+
+        torch.save(net.state_dict(), f'./{model_name}/trained_model.pt')
+        scheduler.step()
+
+        epoch_train_acc, epoch_loss, epoch_val_acc = mean(train_accs), mean(losses), mean(val_accs)
+        epoch_time = round(time.time() - start, 3)
+        writer.add_scalar('Training accuracy', epoch_train_acc, epoch)
+        writer.add_scalar('Training loss', epoch_loss, epoch)
+        writer.add_scalar('Validation accuracy', epoch_val_acc, epoch)
+        writer.add_scalar('Epoch time', epoch_time, epoch)
+        writer.flush()
+
 
 def main():
+    writer = SummaryWriter()
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', required=True)
+    parser.add_argument('--dataset', required=True)
+    parser.add_argument('--lr', default=0.1, type=float)
+    parser.add_argument('--optimizer', default='SGD')
+    parser.add_argument('--weight_decay', default=0.0001, type=float)
+    parser.add_argument('--batch_size', default=256, type=int)
+    parser.add_argument('--num_epochs', default=100, type=int)
     args = parser.parse_args()
-
-    if not os.path.exists('./results/'):
-        os.mkdir('./results/')
+    if not os.path.exists(f'./{args.model_name}'):
+        os.mkdir(f'./{args.model_name}')
     use_amp = True
     scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    net = model.ResNet(3, 224, 1000)
+
+    if args.dataset.lower() == 'cifar10':
+        net = model.ResNet(3, 32, 10)
+        trainset = datasets.CIFAR10(root='../../datasets/cifar10/', train=True)
+        valset = datasets.CIFAR10(root='../../datasets/cifar10/', train=False)
+    elif args.dataset.lower() == 'imagenet':
+        net = model.ResNet(3, 224, 1000)
+        trainset = datasets.ImageNet(root='../../datasets/imagenet/', split='train')
+        valset = datasets.ImageNet(root='../../datasets/imagenet/', split='val')
+    else:
+        raise RuntimeError("Choose either cifar10 or imagenet as dataset")
+
+    writer.add_text('Dataset', args.dataset.lower())
+    writer.add_text('Device', str(device))
+    writer.add_text('Learning rate', str(args.lr))
+    writer.add_text('Optimizer', args.optimizer)
+    writer.add_text('Weight decay', str(args.weight_decay))
+    writer.add_text('Batch size', str(args.batch_size))
+    writer.add_text('Epochs', str(args.num_epochs))
+
     net = net.to(device)
-    train_transforms = transforms.Compose([
-                                            transforms.RandomResize(256, 480),
-                                            transforms.RandomHorizontalFlip(),
-                                            transforms.RandomCrop(224),
-                                            transforms.ColorJitter(0.1),
-                                            transforms.ToTensor(),
-                                            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, num_workers=8,
+                                              pin_memory=False, shuffle=True)
+    valloader = torch.utils.data.DataLoader(valset, batch_size=args.batch_size, num_workers=4)
 
-    test_transforms = transforms.Compose([
-                                            transforms.Resize(256),
-                                            transforms.CenterCrop(224),
-                                            transforms.ToTensor(),
-                                            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-    trainset = torchvision.datasets.ImageNet('../../datasets/imagenet/', transform=train_transforms, split='train')
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, num_workers=8, pin_memory=False, shuffle=True)
-    valset = torchvision.datasets.ImageNet('../../datasets/imagenet/', transform=test_transforms, split='val')
-    valloader = torch.utils.data.DataLoader(valset, batch_size=256, num_workers=4)
-
-    optimizer = torch.optim.Adam(params=net.parameters(), lr=0.001)
-    train(net, trainloader, valloader, device, optimizer, 100, scaler, args.model_name)
-
+    optimizer = torch.optim.SGD(params=net.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.weight_decay)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+    train(net=net, trainloader=trainloader, valloader=valloader, device=device, optimizer=optimizer,
+          num_epochs=args.num_epochs, scheduler=scheduler, model_name=args.model_name, dataset=args.dataset,
+          writer=writer, scaler=scaler)
+    writer.close()
 if __name__ == "__main__":
     main()
